129dacab81a068339524dcdd445ec53592655322
nimowagukari <nimowagukari@outlook.com>
2024-12-12 13:39:02 +0000

    python のコード例を追加

---
+++ src/python-example/tests/test_module1.py
@@ -0,0 +1,11 @@
+from module1 import add
+import unittest
+
+
+class Test_Hoge(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
